cf64e254a03e698a43bb3ec71ca5340cba84bbff
Tomas Ruiz <tomastrg22@gmail.com>
2017-03-07 11:50:14 +0100

    user model added

---
+++ server/api/user/user.model.js
@@ -0,0 +1,18 @@
+/*jshint esversion:6*/
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const userSchema = new mongoose.Schema ({
+  email: { type: String, require: true },
+  password: { type: String, require: true },
+  username: { type: String, require: true },
+  name: { type: String, require: true },
+  description: { type: String, required: false },
+  //language: { type: [String], require: true},
+  createdEvents : [{ type: ObjectId, ref: 'Event' }],
+  assistedEvents: [{ type: ObjectId, ref: 'Event' }],
+  rating: [{ type: ObjectId, ref:'RatingUser' }]
+});
+
+module.exports = mongoose.model('User', userSchema);
