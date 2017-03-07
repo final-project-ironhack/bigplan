b78dadee76907645417ae81c045a2e3c5b040548
Tomas Ruiz <tomastrg22@gmail.com>
2017-03-07 14:31:27 +0100

    user rating added

---
+++ server/api/rating-user/ratingUser.model.js
@@ -0,0 +1,14 @@
+/*jshint esversion:6*/
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const ratingUserSchema = new mongoose.Schema({
+  userId: { type: ObjectId, require: true },
+  punctual: { type: Number, require: true },
+  responsable: { type: Number, require: true },
+  funny: { type: Number, require: true },
+  average: { type: Number, require: true }
+});
+
+module.exports = mongoose.model('ratingUser', ratingUserSchema);
--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require( 'mongoose' );
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   email: { type: String, require: true },
   password: { type: String, require: true },
   username: { type: String, require: true },
