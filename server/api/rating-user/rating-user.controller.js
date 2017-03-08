9d1089ab4b627fc31f6ff7aa798c0dae4d259aa1
Tomas Ruiz <tomastrg22@gmail.com>
2017-03-08 11:10:22 +0100

    API getAllEvents

--- server/api/rating-user/rating-user.controller.js
+++ server/api/rating-user/rating-user.controller.js
@@ -1,3 +1,14 @@
 /*jshint esversion: 6*/
 mongoose = require( ' mongoose' );
 eventModel = require('./ratingUser.model.js');
+
+exports.createRatingUser = (req, res, next) => {
+  const newRatingUser = new ratingEvent({
+    punctual: req.body.punctual,
+    responsable: req.body.responsable,
+    funny: req.body.funny,
+    average: req.body.average
+  });
+
+
+};
